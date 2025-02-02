ba2c6f0426cb17060cf3f1aa1b84862b4f206b0c
Tonda Phan <tondaphan@Tondas-MBP-2.home>
2025-02-02 19:54:29 +0100

    add svg width height

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,10 +4,10 @@ import Menu from '../../../public/menu.svg'
 const Header: FC = ()=>(
   <header className="w-full p-4 left-0 top-0 flex justify-between items-center fixed">
   <div className="w-12 md:w-12 flex items-center">
-  <Logo />
+  <Logo width="100%" height="100%" />
   </div>
   <div className="w-6 md:w-8  flex items-center">
-  <Menu/>
+  <Menu width="100%" height="100%" />
   </div>
 
   </header>
