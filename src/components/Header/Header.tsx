d26f916cea50f3cbe00f357c75a9c2a9f20e3b2b
Tonda Phan <tondaphan@Tondas-MacBook-Pro-2.local>
2025-02-02 17:42:05 +0100

    fix spacing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,11 @@ import React, {FC} from 'react';
 import Logo from '../../../public/logo.svg'
 import Menu from '../../../public/menu.svg'
 const Header: FC = ()=>(
-  <header className="w-full p-11 left-0 top-0 flex justify-between items-center fixed">
-  <div className="w-24 md:w-12 h-24 md:h-12">
+  <header className="w-full p-4 left-0 top-0 flex justify-between items-center fixed">
+  <div className="w-12 md:w-12 flex items-center">
   <Logo />
   </div>
-  <div className="w-12 md:w-8 h-12 md:h-8">
+  <div className="w-6 md:w-8  flex items-center">
   <Menu/>
   </div>
 
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,15 +4,17 @@ import Portrait from '../../public/portrait.jpg'
 const Hero: FC = ()=>(
   <main className="flex justify-center items-center flex-col h-full lg:flex-row">
     <div className="flex justify-center items-center lg:items-start flex-col h-full basis-1/2">
-     <h1 className="text-white text-5xl lg:text-xl" style={{color: "#D4AFB9"}}>Thach Linh Phan</h1>
-     <h4 className="text-white text-center text-8xl lg:text-2xl mb-24 lg:mb-6">Software
+     <div className="flex justify-center items-center lg:items-start flex-col max-w-lg">
+     <h1 className="text-white text-base md:text-4xl" style={{color: "#D4AFB9"}}>Thach Linh Phan</h1>
+     <h4 className="text-white text-center text-4xl md:text-6xl mb-4 lg:mb-6">Software
 Engineer</h4>
-     <p className="text-white text-5xl lg:text-xl text-center lg:text-left leading-normal">Passionate software engineer building usable, scalable solutions. Experienced in Java, TypeScript, Next.js, and Python, with a focus on reliability, testability,</p>
+     <p className="text-white text-xl lg:text-xl text-center lg:text-left leading-normal">Passionate software engineer building usable, scalable solutions. Experienced in Java, TypeScript, Next.js, and Python, with a focus on reliability, testability,</p>
+     </div>
 </div>
 <div className="hidden lg:flex basis-1/2 justify-center items-center">
 <img className="w-3/4 object-cover aspect-square" src={Portrait}/>
 </div>
-<div className="absolute bottom-12 w-24 lg:w-8"><DownIcon />
+<div className="absolute bottom-4 w-8"><DownIcon />
 </div>
 
   </main>
